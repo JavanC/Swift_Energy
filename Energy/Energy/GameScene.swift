650a98db43b7fa1277e27f91811ab532f1f20b35
Javan.Chen <fu6gjp4@gmail.com>
2015-12-01 17:23:02 +0800

    setting up

---
+++ Energy/Energy/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Energy
+//
+//  Created by javan.chen on 2015/12/1.
+//  Copyright (c) 2015年 Javan chen. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
