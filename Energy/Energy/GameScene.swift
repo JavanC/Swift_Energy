2833711146637ac5ebf5e2cd52524229a3df89b4
Javan.Chen <fu6gjp4@gmail.com>
2015-12-02 17:44:59 +0800

    map test

--- Energy/Energy/GameScene.swift
+++ Energy/Energy/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        
+        loadLevel()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,5 +19,57 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func loadLevel() {
+        if let levelPath = NSBundle.mainBundle().pathForResource("level1", ofType: "txt") {
+            if let levelString = try? String(contentsOfFile: levelPath, usedEncoding: nil) {
+                let lines = levelString.componentsSeparatedByString("\n")
+                
+                for (row, line) in lines.reverse().enumerate() {
+                    for (column, letter) in line.characters.enumerate() {
+                        let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 64)
+                        
+                        if letter == "x" {
+                            let node = SKSpriteNode(imageNamed: "block")
+                            node.position = position
+                            
+                            node.physicsBody = SKPhysicsBody(rectangleOfSize: node.size)
+                            node.physicsBody!.dynamic = false
+                            addChild(node)
+                        } else if letter == "v"  {
+                            let node = SKSpriteNode(imageNamed: "vortex")
+                            node.name = "vortex"
+                            node.position = position
+                            node.runAction(SKAction.repeatActionForever(SKAction.rotateByAngle(CGFloat(M_PI), duration: 1)))
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.dynamic = false
+                            
+                            node.physicsBody!.collisionBitMask = 0
+                            addChild(node)
+                        } else if letter == "s"  {
+                            let node = SKSpriteNode(imageNamed: "star")
+                            node.name = "star"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.dynamic = false
+                            
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
+                        } else if letter == "f"  {
+                            let node = SKSpriteNode(imageNamed: "finish")
+                            node.name = "finish"
+                            node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                            node.physicsBody!.dynamic = false
+                            
+                            node.physicsBody!.collisionBitMask = 0
+                            node.position = position
+                            addChild(node)
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
 }
