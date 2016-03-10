f169c55cfe9b0d84f512f4444c55fd502e5b0061
Javan.Chen <fu6gjp4@gmail.com>
2016-03-10 18:09:24 +0800

    change money

--- Energy/Energy/GameViewController.swift
+++ Energy/Energy/GameViewController.swift
@@ -169,9 +169,10 @@ class GameViewController: UIViewController {
         isMusicMute = defaults.boolForKey("isMusicMute")
 
         //1000000000000000
-//        money     = 1000
-        money     = 888000000000000
-        research  = 888000000000000
+        money     = 100000
+//        money     = 888000000000000
+        research  = 100000
+//        research  = 888000000000000
         
         // load maps unlocked
         for count in 0..<6 {
