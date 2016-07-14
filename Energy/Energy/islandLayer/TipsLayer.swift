2dd476789ef75f0aa0ec0cca20e89bfb45c5b2c1
JavanC <fu6gjp4@gmail.com>
2016-07-14 21:27:53 +0800

    add tips image

--- Energy/Energy/islandLayer/TipsLayer.swift
+++ Energy/Energy/islandLayer/TipsLayer.swift
@@ -14,61 +14,22 @@ class TipsLayer: SKNode {
     init(size: CGSize) {
         super.init()
         self.alpha = 0
-
-        let gap = size.height / 25
         
-        let tipsImage = SKSpriteNode(color: colorBlue2, size: size)
+        let background = SKSpriteNode(color: SKColor.blackColor(), size: size)
+        background.name = "background"
+        background.alpha = 0.7
+        addChild(background)
+        
+        let tipsImage = SKSpriteNode(imageNamed: "Tips-EN".localized)
         tipsImage.size = size
         addChild(tipsImage)
-        
-        let tipsLabel = SKLabelNode(fontNamed: "SanFranciscoRounded-Black".localized)
-        tipsLabel.name = "tipsLabel"
-        tipsLabel.text = "Tips"
-        tipsLabel.fontSize = 60 * framescale
-        tipsLabel.verticalAlignmentMode = .Center
-        tipsLabel.position = CGPoint(x: 0, y: gap * 11)
-        addChild(tipsLabel)
-        
-        let imgSize = CGSizeMake(170 * framescale, 170 * framescale)
-        
-        let tipImg_1 = SKSpriteNode(imageNamed: "Tips1")
-        tipImg_1.name = "tip_img_1"
-        tipImg_1.size = imgSize
-        tipImg_1.position = CGPoint(x: -4 * gap, y: 7 * gap)
-        addChild(tipImg_1)
-        
-        let line1 = SKShapeNode(rectOfSize: CGSizeMake(size.width * 0.9, 1 * framescale))
-        line1.name = "line1"
-        line1.lineWidth = 0
-        line1.fillColor = SKColor.whiteColor()
-        line1.position = CGPoint(x: 0, y: 4.15 * gap)
-        addChild(line1)
-        
-        let tipImg_2 = SKSpriteNode(imageNamed: "Tips2")
-        tipImg_2.name = "tip_img_1"
-        tipImg_2.size = imgSize
-        tipImg_2.position = CGPoint(x: -4 * gap, y: 1.3 * gap)
-        addChild(tipImg_2)
-        
-        let line2 = SKShapeNode(rectOfSize: CGSizeMake(size.width * 0.9, 1 * framescale))
-        line2.name = "line1"
-        line2.lineWidth = 0
-        line2.fillColor = SKColor.whiteColor()
-        line2.position = CGPoint(x: 0, y: -1.55 * gap)
-        addChild(line2)
-        
-        let tipImg_3 = SKSpriteNode(imageNamed: "Tips3")
-        tipImg_3.name = "tip_img_1"
-        tipImg_3.size = imgSize
-        tipImg_3.position = CGPoint(x: -4 * gap, y: -4.4 * gap)
-        addChild(tipImg_3)
         
         OKButton = SKShapeNode(circleOfRadius: 40 * framescale)
         OKButton.name = "OKButton"
         OKButton.fillColor = SKColor.clearColor()
         OKButton.strokeColor = SKColor.whiteColor()
         OKButton.lineWidth = 3 * framescale
-        OKButton.position = CGPoint(x: 0, y: -9 * gap)
+        OKButton.position = CGPoint(x: 0, y: -size.height / 2 + 100 * framescale)
         OKButton.zPosition = 1
         let checkImg = SKSpriteNode(texture: iconAtlas.textureNamed("check"))
         checkImg.size = CGSizeMake(40 * framescale, 40 * framescale)
